9a5d4ea5923e87c337bf8182bd0bb4f5b1fa0374
Paul "Rohja" LESELLIER <rohja@rohja.com>
2021-03-05 17:22:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,7 @@ git_cmd() {
   fi
 }
 
-PR_BRANCH="auto-$GITHUB_SHA"
+PR_BRANCH="production-cherry-pick-$GITHUB_SHA"
 MESSAGE=$(git log -1 $GITHUB_SHA | grep "AUTO" | wc -l)
 
 if [[ $MESSAGE -gt 0 ]]; then
@@ -39,4 +39,4 @@ git_cmd git fetch --all
 git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
 git_cmd git cherry-pick "${GITHUB_SHA}"
 git_cmd git push -u origin "${PR_BRANCH}"
-git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
+git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"${PR_TITLE}\""
